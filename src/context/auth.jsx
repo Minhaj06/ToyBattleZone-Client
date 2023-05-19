d1047d937f8aa3be993de63528344ca94ce44f54
Minhaj06 <milonmdminhajkobir@gmail.com>
2023-05-19 11:58:16 +0600

    Firebase Credentials in .env

---
+++ src/context/auth.jsx
@@ -0,0 +1,61 @@
+import React, { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
+
+export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    setIsLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const logIn = (email, password) => {
+    setIsLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOut = () => {
+    setIsLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (loggedUser) => {
+      setUser(loggedUser);
+      setIsLoading(false);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const loading = (value) => {
+    setIsLoading(value);
+  };
+
+  const authInfo = {
+    user,
+    isLoading,
+    loading,
+    createUser,
+    logIn,
+    logOut,
+  };
+
+  return <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>;
+};
+
+export default AuthProvider;
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,18 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCiVwdQjQrwG7haJweCk7OF_yFL9YuDkts",
-  authDomain: "toybattlezone.firebaseapp.com",
-  projectId: "toybattlezone",
-  storageBucket: "toybattlezone.appspot.com",
-  messagingSenderId: "1045528839740",
-  appId: "1:1045528839740:web:2cab53d5cb2277a2652539",
+  //   apiKey: "AIzaSyCiVwdQjQrwG7haJweCk7OF_yFL9YuDkts",
+  //   authDomain: "toybattlezone.firebaseapp.com",
+  //   projectId: "toybattlezone",
+  //   storageBucket: "toybattlezone.appspot.com",
+  //   messagingSenderId: "1045528839740",
+  //   appId: "1:1045528839740:web:2cab53d5cb2277a2652539",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_MESSAGING_APP_ID,
 };
 
 // Initialize Firebase
