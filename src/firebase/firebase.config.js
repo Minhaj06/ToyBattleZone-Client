5edd7f01b6aa3ea861fd95a8a3163bac923e0d66
Minhaj06 <milonmdminhajkobir@gmail.com>
2023-05-19 11:52:10 +0600

    Successfully deployed to firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCiVwdQjQrwG7haJweCk7OF_yFL9YuDkts",
+  authDomain: "toybattlezone.firebaseapp.com",
+  projectId: "toybattlezone",
+  storageBucket: "toybattlezone.appspot.com",
+  messagingSenderId: "1045528839740",
+  appId: "1:1045528839740:web:2cab53d5cb2277a2652539",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
